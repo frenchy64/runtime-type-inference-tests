ea44ec87db83febf39d68a58741616b28d1a0dbb
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-10-22 13:30:15 -0400

    run all

--- run-all.sh
+++ run-all.sh
@@ -1,17 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-cd startrek-clojure
-./run.sh
-cd ../data.xml
-./run.sh
-cd ../data.json
-./run.sh
-cd ../fs
-./run.sh
-cd ../math.combinatorics
-./run.sh
-cd ../java.jdbc
-./run.sh
+BRANCH_NAME=infer-types
+
+function git_branch {
+  git branch -D $BRANCH_NAME
+  git fetch origin $BRANCH_NAME
+  git checkout -b $BRANCH_NAME origin/$BRANCH_NAME
+  git checkout -B $BRANCH_NAME
+}
+
+function git_push {
+  git add src
+  git commit -m "infer types"
+  git push origin $BRANCH_NAME:$BRANCH_NAME
+}
+
+function run_tests {
+  cd $1
+  git_branch
+  ./run.sh
+  git_push
+  cd ..
+}
+
+run_tests startrek-clojure
+run_tests data.xml
+run_tests data.json
+run_tests fs
+run_tests math.combinatorics
+#cd ../java.jdbc
+#./run.sh
 # requires a lot of memory
 #cd ../data.int-map
 #./run.sh
