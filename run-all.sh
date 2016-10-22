f7f747209ae002f52446c211dcc725d69eeb036e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-10-22 17:53:52 -0400

    run-all

--- run-all.sh
+++ run-all.sh
@@ -6,15 +6,15 @@ BRANCH_NAME=infer-types
 # will eventually track origin/$BRANCH_NAME
 function git_branch {
   git branch -D $BRANCH_NAME
-  git fetch origin $BRANCH_NAME
-  git checkout -b $BRANCH_NAME origin/$BRANCH_NAME
+  #git fetch origin $BRANCH_NAME
+  #git checkout -b $BRANCH_NAME origin/$BRANCH_NAME
   git checkout -B $BRANCH_NAME
 }
 
 function git_push {
   git add src
   git commit -m "infer types"
-  git push origin $BRANCH_NAME:$BRANCH_NAME
+  git push origin $BRANCH_NAME:$BRANCH_NAME -f
 }
 
 function run_tests {
@@ -30,8 +30,7 @@ run_tests data.xml
 run_tests data.json
 run_tests fs
 run_tests math.combinatorics
-#cd ../java.jdbc
-#./run.sh
+run_tests java.jdbc
 # requires a lot of memory
 #cd ../data.int-map
 #./run.sh
